26c6a6de8199e10ccba0988972cdd612d71c2b3c
Ayesha <32429595+bladedancer-256@users.noreply.github.com>
2018-12-11 23:37:46 +1100

    Update 1_1_IsUnique.py
    Added optimization of ASCII character count checking

--- 1_1_IsUnique.py
+++ 1_1_IsUnique.py
@@ -1,6 +1,9 @@
 #Determine if a string has all unique characters 
 import sys
 def remove_with_dict(string):
+    #128 unique characters in ASCII, so if string is longer than that, it can't be unique
+    if len(string) > 128:
+        return False 
     string_dict = {}
     for letter in string:
         if letter not in string_dict:
